0ba01b1d6892042bc9adaf5eb9c25647d44c94ea
banukumar7 <banusanthira@gmail.com>
2025-06-21 12:22:15 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.Data;
+using WindowsFormsApp1.View;
+
+namespace WindowsFormsApp1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            CreateTables.CreateTable();
+            using (var con = new SQLiteConnection("Data Source=UNICOM.db"))
+            {
+                con.Open();
+                DBInitializer.SeedUsers(con);
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginForm());
+
+            
+
+        }
+    }
+}
