13541e539c8466609621e6e08cec2197958a5555
banukumar7 <banusanthira@gmail.com>
2025-06-24 11:48:06 +0530

    Add files via upload

---
+++ bin/Debug/Model/Exam.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Model
+{
+    internal class Exam
+    {
+        public string Subject { get; set; }
+        public DateTime ExamDate { get; set; }
+    }
+}
---
+++ bin/Debug/Model/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Model
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int CourseID { get; set; }
+        public string CourseName { get; set; }
+    }
+}
